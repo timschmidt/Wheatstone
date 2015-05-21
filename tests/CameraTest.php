d37042cf6f3366aa34a3590ef38e4672b9739f6e
Travis Hyyppä <travishyyppa@gmail.com>
2015-05-21 15:20:07 -0600

    Camera test position and resolution
    Signed-off-by: Travis Hyyppä <travishyyppa@gmail.com>

--- tests/CameraTest.php
+++ tests/CameraTest.php
@@ -37,4 +37,40 @@ class CameraTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals( 20, $this->camera->rotation->z );
     }
 
+
+    public function testPosition()
+    {
+        $this->camera->position( 10, 10, 10 );
+        $this->assertEquals( 10, $this->camera->position->x );
+        $this->assertEquals( 10, $this->camera->position->y );
+        $this->assertEquals( 10, $this->camera->position->z );
+
+        $this->camera->position( 10, 0, 0 );
+        $this->assertEquals( 20, $this->camera->position->x );
+        $this->assertEquals( 10, $this->camera->position->y );
+        $this->assertEquals( 10, $this->camera->position->z );
+
+        $this->camera->position( 0, 0, 10 );
+        $this->assertEquals( 20, $this->camera->position->x );
+        $this->assertEquals( 10, $this->camera->position->y );
+        $this->assertEquals( 20, $this->camera->position->z );
+
+        $this->camera->position( 0, 20, 0 );
+        $this->assertEquals( 20, $this->camera->position->x );
+        $this->assertEquals( 30, $this->camera->position->y );
+        $this->assertEquals( 20, $this->camera->position->z );
+
+        $this->camera->position( -100, -100, -100 );
+        $this->assertEquals( -80, $this->camera->position->x );
+        $this->assertEquals( -70, $this->camera->position->y );
+        $this->assertEquals( -80, $this->camera->position->z );
+    }
+
+
+    public function testHasResolution()
+    {
+        $this->assertNotNull( $this->camera->width );
+        $this->assertNotNull( $this->camera->height );
+    }
+
 }
