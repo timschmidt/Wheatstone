189692f6e0b71bd833453a86f3c3a159b6438add
Travis Hyyppä <travishyyppa@gmail.com>
2015-05-21 15:37:43 -0600

    Png Exporter test
    Signed-off-by: Travis Hyyppä <travishyyppa@gmail.com>

---
+++ tests/Export/PngExporterTest.php
@@ -0,0 +1,39 @@
+<?php
+
+
+use Thyyppa\Wheatstone\Object\Point;
+
+class PngExporterTest extends PHPUnit_Framework_TestCase {
+
+    private $exporter;
+
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->exporter = new \Thyyppa\Wheatstone\Export\PngExporter( 100, 100 );
+    }
+
+
+    public function testLineDraw()
+    {
+        $a = Mockery::mock( Point::class );
+        $b = Mockery::mock( Point::class );
+
+        $this->exporter->line( $a, $b );
+    }
+
+
+    public function testFileOutput()
+    {
+        $this->testLineDraw();
+
+        $this->exporter->output( './tests/test.png' );
+
+        $this->assertFileExists( './tests/test.png' );
+
+        unlink( './tests/test.png' );
+    }
+
+}
